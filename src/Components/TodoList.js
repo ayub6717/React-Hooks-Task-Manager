f2e3c2f1f2628aeba5fafbef415a0e57affa59ba
ayub6717 <ayub15-6717@diu.edu.bd>
2021-02-28 13:11:27 +0600

    Task manager

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo';
 import TodoForm from "./TodoForm";
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -9,13 +10,44 @@ function TodoList() {
         if(!todo.text || /^\s*$/.test(todo.text)){
             return
         }
-        const newTodo = [todo, ...todos]
-        setTodos(newTodo)
-    }
+        const newTodo = [todo, ...todos];
+        setTodos(newTodo);
+        console.log(...todos);
+    };
+
+    const updateTodo = (todoId, newValue) => {
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+          return;
+        }
+    
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+      };
+
+    const removeTodo = id => {
+        const removedArr = [...todos].filter(todo => todo.id !== id);
+    
+        setTodos(removedArr);
+      };
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+          if (todo.id === id) {
+            todo.isComplete = !todo.isComplete;
+          }
+          return todo;
+        });
+        setTodos(updatedTodos);
+      };
     return (
         <div>
             <h1>Add Your Task</h1>
             <TodoForm onSubmit={addTodo} />
+            <Todo 
+                todos={todos} 
+                completeTodo = {completeTodo}
+                removeTodo={removeTodo}
+                updateTodo={updateTodo}
+            />
         </div>
     )
 }
